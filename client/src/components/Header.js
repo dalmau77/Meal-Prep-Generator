2052d31fe50c2c64dc00951ed70c4d040aa6ea4f
Eric Dalmau <dalmaueric29@gmail.com>
2020-04-13 14:07:54 -0700

    login button works fully

---
+++ client/src/components/Header.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom'
+
+class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return (
+          <li><a href='/auth/google'>Login With Google</a></li>
+        )
+      default:
+        return <li><a href='/api/logout'>Logout</a></li>
+    }
+  }
+  render() {
+    return (
+      <nav>
+        <div className='nav-wrapper  light-blue darken-4'>
+          <Link
+            to={this.props.auth ? '/surveys': '/'}
+            className='left brand-logo'>
+            Meal Prepper
+        </Link>
+          <ul className='right grey darken-3'>
+            {this.renderContent()}
+          </ul>
+        </div>
+      </nav>
+    )
+  }
+}
+
+function mapStateToProps({ auth }) {
+  return { auth }
+}
+
+export default connect(mapStateToProps)(Header)
---
+++ client/src/components/Landing.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class Landing extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Are you bored of eating the same thing everyday
+          ?
+        </h1>
+        try out our meal generator
+      </div>
+    )
+  }
+}
